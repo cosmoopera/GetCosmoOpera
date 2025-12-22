c5e313dd4d7f45b46809003969e6f951ca4d68cd
cosmoopera <email7999@gmail.com>
2025-12-22 18:54:42 +0300

    sync 22.12.2025 18:54:42,15

--- gitsync.bat
+++ gitsync.bat
@@ -1,3 +1,9 @@
+@echo off
+cd /d C:\WIKI-CLEAN
 git add .
-git commit -m "обновление файла"
+git commit -m "sync %date% %time%" || exit /b 0
 git push
+echo.
+echo ========= СИНХРОНИЗАЦИЯ ГОТОВА =========
+echo Мобильный Fit: Manual Pull (котик)
+pause
